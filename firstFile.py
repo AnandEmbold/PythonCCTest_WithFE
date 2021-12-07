06a549c5f1499cf5a743b969dfa104c0b329244c
AnandEmbold <80742162+AnandEmbold@users.noreply.github.com>
2021-12-07 12:34:22 +0530

    Initial commit

---
+++ firstFile.py
@@ -0,0 +1,56 @@
+import os
+import subprocess
+import filecmp
+from tqdm import tqdm
+import numpy as np
+import pickle as pkl
+from src.graph.utils import load_vgraph_db, load_target_db
+from src.matching.triplet_match import *
+from multiprocessing import Pool,Process, Queue, SimpleQueue
+import time
+
+def decision_function(cvg_score, pvg_score, nvg_score):
+    return cvg_score >= CVG_THRESH and pvg_score >= PVG_THRESH and pvg_score > nvg_score
+
+#def consume(work):
+#    (target_id, vg, t_trips) = work
+#    cvg_score, pvg_score, nvg_score = triplet_match_exact(vg, t_trips)
+#    return (target_id, vg, cvg_score, pvg_score, nvg_score)
+#    # random comment
+    
+class A:
+        # random comment
+    # random comment
+
+    def try():
+        x = 'x' # input()
+        if x == '':
+                # random comment
+
+            x = 'a' # print("a")
+        elif x == '':
+            # random comment
+
+            x = 'b' # print("b")
+        else:
+            x = 'c' # print("c")
+
+            
+def try():
+    x = 'x' # input()
+    if x == '':
+        x = 'a' # print("a")
+    elif x == '':
+        x = 'b' # print("b")
+    else:
+        x = 'c' # print("c")
+
+        
+        
+x = 'x' # input()
+if x == '':
+    x = 'a' # print("a")
+# elif x == '':
+#    x = 'b' # print("b")
+else:
+    x = 'c' # print("c")
